39e18edc0f61ba35febe6b3dae1d7f82251bae0d
A-B-Legostaev <160860618+A-B-Legostaev@users.noreply.github.com>
2024-06-06 13:23:54 +0300

    Add files via upload

---
+++ 4.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <cmath>
+#include <cstring>
+using namespace std;
+
+/**
+* @brief вычисление функции из нашего примера
+* @param x число х
+* @return результат функции
+*/
+double f(const double x);
+
+/**
+* @brief Считывание параметра типа double из stdin
+* @return результат считывания
+*/
+double getValue();
+
+/**
+* @brief Проверка на неотрицательное число
+* @param value число для проверки
+*/
+void checkPositive(const double value);
+
+
+/**
+* @brief точка входа в программу
+* @return 0
+*/
+int main() {
+    cout << "х начальный:" << endl;
+    double x = getValue();
+    cout << "х максимальный:" << endl;
+    double x_max = getValue();
+    cout << "Δ:" << endl;
+    double plus_x = getValue();
+
+
+    checkPositive(plus_x);
+    if (x_max < x) {
+        cout << "x_max < x_min" << endl;
+        abort();
+    }
+    for (double i = x; i < x_max + plus_x; i += plus_x) {
+        if ((1.0- i) < 0.0 || cos(i) == 0.0) {
+            cout <<i<<"=неопределено, (выражение под корнем должно быть больше или равно нулю, а так же тангенс не определен в точках где косинус равен нулю)"<<endl;
+        } else {
+            cout <<i<<"="<<f(i)<<endl;
+        }
+    }
+    return 0;
+}
+
+double f(const double x) {
+    return sqrt(1-x)-tan(x);
+}
+
+double getValue() {
+    double value;
+    cin >> value;
+    if (cin.fail()) {
+        cout << "Некорректное значение" << endl;
+        abort();
+    }
+    return value;
+}
+
+void checkPositive(const double value) {     
+    if (value <= 0) {     
+        cout << "Error" << endl;
+        abort();
+    }     
+}
